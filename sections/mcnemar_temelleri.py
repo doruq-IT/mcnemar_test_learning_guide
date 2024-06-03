045bc6b605eea33cb9a134df19ddf53bfcddb4f3
Okan KURTAR <dorokozmer@gmail.com>
2024-06-03 23:08:44 +0300

    update

--- sections/mcnemar_temelleri.py
+++ sections/mcnemar_temelleri.py
@@ -5,5 +5,7 @@ def goster_temeller():
     st.write("""
     McNemar testi, bağımlı iki ikili veri kümesi arasındaki oranları karşılaştırmak için kullanılır. Özellikle aynı bireylerin farklı zamanlardaki veya koşullardaki sonuçlarını analiz etmek için idealdir.
     - **Bağımlı Örnekler:** Aynı grup, farklı zaman veya koşullarda test edilir.
+                            Bir grup hastanın tedavi öncesi ve tedavi sonrası sağlık durumlarını karşılaştırmak.
     - **Eşleştirilmiş Veriler:** Aynı bireylerin iki farklı ölçümdeki sonuçları karşılaştırılır.
+                                 Aynı öğrencilerin iki farklı sınavdaki başarı durumlarını karşılaştırmak.
     """)
