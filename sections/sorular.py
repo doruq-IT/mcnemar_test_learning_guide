ad0270c65d9a8917f30b3d24ec731db27f7a494e
Okan KURTAR <dorokozmer@gmail.com>
2024-06-03 19:43:44 +0300

    update

--- sections/sorular.py
+++ sections/sorular.py
@@ -67,7 +67,7 @@ chi_square = (b - c) ** 2 / (b + c)
 p_value_chi2 = chi2.sf(chi_square, 1)
 
 # Binom testi
-p_value_binom = binom_test([b, c])
+p_value_binom = binomtest([b, c])
 
 print('Chi-square Statistic:', chi_square)
 print('p-value (Chi-square):', p_value_chi2)
